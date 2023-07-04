2a065c3f8a950f10607c71adf767085c4359b4f4
Charu Srivastava <charusrivastava@HL00793.local>
2023-07-04 15:25:12 +0900

    Changes address

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ function speak() {
     var speechLanguage = document.getElementById('speechLanguage').value;
 
     // first call the translate API
-    fetch('http://127.0.0.1:5000/translate', {
+    fetch('http://43.206.222.228:5000/translate', {
       
         method: 'POST',
         headers: {
@@ -17,7 +17,7 @@ function speak() {
             var translatedText = data.translated_text;
 
             // then call the speak API with the translated text
-            fetch('http://127.0.0.1:5000/speak', {
+            fetch('http://43.206.222.228:5000/speak', {
         
                 method: 'POST',
                 headers: {
@@ -48,7 +48,7 @@ function translate() {
     var text = document.getElementById('translateText').value;
     var sourceLanguage = document.getElementById('sourceLanguage').value;
     var targetLanguage = document.getElementById('targetLanguage').value;
-    fetch('http://127.0.0.1:5000/translate', {
+    fetch('http://43.206.222.228/translate', {
        
         method: 'POST',
         headers: {
@@ -107,7 +107,7 @@ function uploadImage() {
 
     formData.append("language", imgLanguage);
 
-    fetch('http://127.0.0.1:5000/upload', {
+    fetch('http://43.206.222.228/upload', {
         method: 'POST',
         body: formData
     })
